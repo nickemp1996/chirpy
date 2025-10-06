11061c2909c99832e6aa6b8aecac1b81e5a9358e
nickemp1996 <nickemp1996@gmail.com>
2025-10-05 21:40:51 -0600

    Added error checking and "bad word replacement" functions to a new "validate chirp" api

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"os"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -12,16 +16,21 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
 
 func (cfg *apiConfig) getFileserverHits(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	message := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	message := fmt.Sprintf(`<html>
+							  <body>
+							    <h1>Welcome, Chirpy Admin</h1>
+							    <p>Chirpy has been visited %d times!</p>
+							  </body>
+							</html>`, cfg.fileserverHits.Load())
 	_, err := w.Write([]byte(message))
 	if err != nil {
 		fmt.Printf("Error writing response: %v", err)
@@ -42,19 +51,102 @@ func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	type returnVals struct {
+		// the key will be the name of struct field unless you give it an explicit JSON tag
+		Error string `json:"error"`
+	}
+	respBody := returnVals{
+		Error: msg,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+
+	return nil
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+
+	return nil
+}
+
+func replaceBadWords(body string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(body, " ")
+
+	for i, word := range words {
+		if slices.Contains(badWords, strings.ToLower(word)) {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		err = respondWithError(w, 400, "chirp too long")
+	} else {
+		type returnVals struct {
+			// the key will be the name of struct field unless you give it an explicit JSON tag
+			CleanedBody string `json:"cleaned_body"`
+		}
+		respBody := returnVals{
+			CleanedBody: replaceBadWords(params.Body),
+		}
+
+		err = respondWithJSON(w, 200, respBody)
+	}
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 	apiCfg := &apiConfig{}
 
 	server := &http.Server{
-		Addr:         ":8080",
-		Handler:      mux,
+		Addr:    ":8080",
+		Handler: mux,
 	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/healthz", readinessEndpoint)
-	mux.HandleFunc("/metrics", apiCfg.getFileserverHits)
-	mux.HandleFunc("/reset", apiCfg.resetFileserverHits)
+	mux.HandleFunc("GET /api/healthz", readinessEndpoint)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.getFileserverHits)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetFileserverHits)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	fmt.Println("Starting server on ", server.Addr)
 	err := server.ListenAndServe()
